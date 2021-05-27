acbfe102b959ef5884ce47258037cd1ba80cb4e5
mdestafadilah <desta.08b@gmail.com>
2021-05-27 10:36:47 +0700

    Fake Data Soal

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,29 @@
+export const Questions = [
+    {
+        soal: "Berapa 1+3?",
+        optionA: "5",
+        optionB: "3",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+
+    {
+        soal: "Siapa Programmer Paling Ganteng?",
+        optionA: "Desta",
+        optionB: "Rohim",
+        optionC: "Farhan",
+        optionD: "Rizal",
+        answer: "A",
+    },
+
+    {
+        soal: "Siapa Programmer Tukang Obat?",
+        optionA: "Rohim",
+        optionB: "Rizal",
+        optionC: "Farhan",
+        optionD: "Aris",
+        answer: "D",
+    },
+
+]
