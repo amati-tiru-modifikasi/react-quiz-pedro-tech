5cf3193cc210d45d93d6a9e521de86e46abc3501
mdestafadilah <desta.08b@gmail.com>
2021-05-27 10:37:10 +0700

    Hooks Menampilkan soal dan pilihan

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Soal
+import { Questions } from '../Helpers/QuestionBank'
 
 export default function Quiz() {
+
+    // Cek Soal
+    const [currQuestion, setCurrQuestion] = useState(1);
+
+    // Cek Pilihan
+    const [optionChosen, setOptionChosen] = useState('');
+
     return (
-        <div>
-            Quis
+        <div className="Quiz">
+            <h1>{Questions[currQuestion].soal}</h1>
+            <div className="options">
+                <button onClick={() => setOptionChosen('A')}>{Questions[currQuestion].optionA}</button>
+                <button onClick={() => setOptionChosen('B')}>{Questions[currQuestion].optionB}</button>
+                <button onClick={() => setOptionChosen('C')}>{Questions[currQuestion].optionC}</button>
+                <button onClick={() => setOptionChosen('D')}>{Questions[currQuestion].optionD}</button>
+            </div>
+            <button>Next</button>
         </div>
     )
 }
