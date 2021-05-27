c8a03a2ae39c5635db5a28def85540f700c2ec55
mdestafadilah <desta.08b@gmail.com>
2021-05-27 10:45:02 +0700

    Next soal

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { QuizContext } from '../Helpers/Contexts'
 
 // Soal
 import { Questions } from '../Helpers/QuestionBank'
 
 export default function Quiz() {
 
+    const { score, setScore } = useContext(QuizContext);
+
     // Cek Soal
-    const [currQuestion, setCurrQuestion] = useState(1);
+    const [currQuestion, setCurrQuestion] = useState(0);
 
     // Cek Pilihan
     const [optionChosen, setOptionChosen] = useState('');
+
+    const nextQuestion = () => {
+        if(Questions[currQuestion].answer == optionChosen) {
+            setScore(score + 1)
+        }
+        console.log(score)
+        setCurrQuestion(currQuestion+1)
+    }
 
     return (
         <div className="Quiz">
@@ -20,7 +31,7 @@ export default function Quiz() {
                 <button onClick={() => setOptionChosen('C')}>{Questions[currQuestion].optionC}</button>
                 <button onClick={() => setOptionChosen('D')}>{Questions[currQuestion].optionD}</button>
             </div>
-            <button>Next</button>
+            <button onClick={() => nextQuestion()}>Next</button>
         </div>
     )
 }
