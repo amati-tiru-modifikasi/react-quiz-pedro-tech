ae0e27f1ee93df268f1ef0cb6406626f62d9552f
mdestafadilah <desta.08b@gmail.com>
2021-05-27 11:25:02 +0700

    Ending Context Hook

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuizContext } from '../Helpers/Contexts'
+import { Questions } from '../Helpers/QuestionBank'
+import "../App.css"
 
 export default function EndScreen() {
+
+    const { score, setScore, setGameState } = useContext(QuizContext);
+
+    const restartQuiz = () => {
+        setScore(0);
+        setGameState('menu');
+    }
+
     return (
-        <div>
-            Result
+        <div className="EndScreen">
+            <h1>Kuis Selesai</h1>
+            <h3> {score} / {Questions.length}</h3>
+            <button onClick={() => restartQuiz()}>Restart Kuis</button>
         </div>
     )
 }
